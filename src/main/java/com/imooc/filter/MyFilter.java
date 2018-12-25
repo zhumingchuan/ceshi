d517a2a7c736d85ad1750b22b36a210d194ce6a9
zhumingchuan <1556518588@qq.com>
2018-12-25 14:47:35 +0800

    Default Changelist

--- src/main/java/com/imooc/filter/MyFilter.java
+++ src/main/java/com/imooc/filter/MyFilter.java
@@ -22,7 +22,7 @@ public class MyFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse res, FilterChain chan) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
-        System.out.println("2222222222223333");
+        System.out.println("22222222222233");
         HttpHeaders headers = new HttpHeaders();
         String authorization = req.getHeader("Authorization");
         String name = request.getParameter("name");
