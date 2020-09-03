24c3d7b0ba9e5ca11881f8f489e7f06c63d4d2e8
itcast <51zxw@qq.com>
2020-09-03 11:49:47 +0800

    hhhhhhh

--- src/main/java/com/imooc/web/HomeController.java
+++ src/main/java/com/imooc/web/HomeController.java
@@ -21,6 +21,11 @@ public class HomeController {
 
     @GetMapping("/500")
     public String internalError() {
+        System.out.println("测试github开始");
+        System.out.println("测试github开始");
+        System.out.println("测试github开始");
+        System.out.println("测试github开始");
+        System.out.println("测试github开始");
         return "500";
     }
     @GetMapping("/logout/page")
